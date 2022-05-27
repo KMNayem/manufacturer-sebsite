32c70dfb4641c0858b418697c25a6f12d5cb1c86
KMNayem <imna41695@gmail.com>
2022-05-27 15:14:00 +0600

    quantity

--- src/Pages/OrderDetails/OrderDetails.js
+++ src/Pages/OrderDetails/OrderDetails.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import OrderForm from '../OrderForm/OrderForm';
 
 const OrderDetails = () => {
 
      const {productId} = useParams();
      const [product, setProduct] = useState({});
      const {_id, quantity, balance, picture, name, about} = product;
+
+     
+
 
      useEffect(() =>{
          const url =`http://localhost:5000/product/${productId}`;
@@ -28,37 +32,11 @@ const OrderDetails = () => {
                 <h4>Quantity: {quantity}</h4>
                 <h4>Price: ${balance} per pcs</h4>
                 <h4>Minimum Order: 50 pcs</h4>
-                <h2 className='mt-3 font-bold text-2xl '><u>Order Form</u></h2>
-
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">Your Name</span>
-                    </label>
-                        <input type="text" placeholder="Name Here" class="input input-bordered w-full max-w-xs" />
-                </div>
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">Your Email</span>
-                    </label>
-                        <input type="text" placeholder="Email here" class="input input-bordered w-full max-w-xs" />
-                </div>
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">Quantity</span>
-                    </label>
-                        <input type="number" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                </div>
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">Phone Number</span>
-                    </label>
-                        <input type="number" placeholder="Number here" class="input input-bordered w-full max-w-xs" />
-                </div>
+                
+                <OrderForm></OrderForm>
                
 
-                <div class="card-actions mt-3">
-                    <button class="btn btn-primary">Confirm Order</button>
-                </div>
+                
             </div>
         </div>
         <div class="card w-96 mt-10  max-w-sm mx-auto bg-base-100 shadow-xl">
---
+++ src/Pages/OrderForm/OrderForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+
+
+const OrderForm = () => {
+
+    const minValue =( that, value )=>{
+        const min = parseInt(that.getAttribute('min'))
+        const val = parseInt(value);
+        if(val < min || isNaN(val)){
+            return alert('please order at least 50 product..');
+        }
+        else{
+            return val;
+        }
+    }
+
+    const [user, loading, error] = useAuthState(auth);
+
+    return (
+        <div className='mt-5'>
+            <h2 className='mt-3 font-bold text-2xl '><u>Order Form</u></h2>
+            <div class="form-control mt-3 w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Your Name</span>
+                </label>
+                <input type="text" disabled value={user?.displayName || ''} placeholder="Name Here" class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Your Email</span>
+                </label>
+                <input type="text"  disabled value={user?.email || ''} placeholder="Email here" class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div class="form-control w-full max-w-xs">
+                <label class="label mt-2">
+                    <span class="label-text">Quantity :</span>
+                    <input type="number" onKeyUp={minValue} min="50" placeholder="Type here" class="input input-bordered w-24 max-w-xs" />
+                </label>
+                
+            </div>
+                <div class="form-control w-full max-w-xs">
+                <label class="label">
+                    <span class="label-text">Phone Number</span>
+                </label>
+                <input type="number" placeholder="Number here" class="input input-bordered w-full max-w-xs" />
+                </div>
+                <div class="mt-4">
+                    <button class="btn btn-primary">Confirm Order</button>
+                </div>
+        </div>
+    );
+};
+
+export default OrderForm;
